8afa3d8a5ca7ae2dc6ba38994e0d6c7aa1f49654
mjduvall <mjduvall@duvallglobal.com>
2025-03-23 05:07:01 -0400

    Start Managing YOUR Fans Now!

---
+++ src/types/gtag.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for Google Analytics gtag.js
+interface Window {
+  gtag: (
+    command: string,
+    action: string,
+    params: {
+      event_category?: string;
+      event_label?: string;
+      [key: string]: any;
+    }
+  ) => void;
+}
